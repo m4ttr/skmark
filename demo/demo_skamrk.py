b39833386c510ac67ebf9f39ddb3c3cf247b8ac5
Matt Rivet <rivet@uber.com>
2017-08-29 17:19:54 +0000

    include a demo

--- demo/demo_skamrk.py
+++ demo/demo_skamrk.py
@@ -3,25 +3,26 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('10.30.23.31', 5555)
+server_address = (socket.gethostname, 5555)
 sock.bind(server_address)
-
 sock.listen(1)
 
 while True:
         print >>sys.stderr, 'waiting for a connection'
-        connection, client_address = sock.accept()
+        connection, client_addr = sock.accept()
         try:
-            print >>sys.stderr, 'connection from', client_address
+            print >>sys.stderr, 'connection from', client_addr
             while True:
                 data = connection.recv(16)
                 print >>sys.stderr, 'received "%s"' % data
                 if data:
                     print >>sys.stderr, 'sending data back to the client'
                     connection.sendall(data)
-                    print >> sys.stderr, connection.getsockopt(socket.SOL_SOCKET, 36)
+                    print >>sys.stderr, connection.getsockopt(
+                            socket.SOL_SOCKET, 36
+                    )
                 else:
-                            print >>sys.stderr, 'no more data from', client_address
-                            break
+                    print >>sys.stderr, 'no more data from', client_addr
+                    break
         finally:
             connection.close()
--- setup/install_build_deps.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install build-essential pkg-config dkms linux-headers-$(uname -r) iptables-dev
-apt-get install module-assistant;
-m-a prepare;
